f8440b50b9ab79129253355535bde17fc4d90d20
kalendae <taoyge@gmail.com>
2009-04-11 05:15:40 +0000

    signup login logout image listing users

---
+++ app/controllers/start_controller.rb
@@ -0,0 +1,8 @@
+class StartController < ApplicationController
+  # Be sure to include AuthenticationSystem in Application Controller instead
+  include AuthenticatedSystem
+  
+  def index
+  end
+  
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,22 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def super_user?
+    current_user and (current_user.login == 'tao' or current_user.login == 'kalendae' or current_user.login == 'scott')
+  end
+
+  def gravatar_url_for(user, options = {})
+    if user.avatar_url.blank?
+      if (ENV) and (ENV['HOSTNAME']) and (ENV['HOSTNAME'].starts_with? 'vmcentos')
+        "/images/default_avatar.jpg"
+      else
+        url_for({ :gravatar_id => Digest::MD5.hexdigest(user.email), :host => 'www.gravatar.com',
+                  :protocol => 'http://', :only_path => false, :controller => 'avatar.php', 'd' => 'http://www.jabberat.com/images/default_avatar.jpg'
+                }.merge(options))
+      end
+    else
+      user.avatar_url
+    end
+  end
+
 end
